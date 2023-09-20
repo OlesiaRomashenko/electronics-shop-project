694f746cfa51695c61d0b668c9db406aa920e5f9
Olesia_Rom <olesya.einstein@gmail.com>
2023-09-20 17:01:03 +0200

    Updates

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,24 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+import pytest
+
+
+@pytest.fixture
+def class_instance():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_init(class_instance):
+    assert class_instance.name == "Смартфон"
+    assert class_instance.price == 10000
+    assert class_instance.quantity == 20
+    assert class_instance is class_instance.__class__.all[0]
+
+
+def test_calculate_total_price(class_instance):
+    assert class_instance.calculate_total_price() == 200000
+
+
+def test_apply_discount(class_instance):
+    assert class_instance.apply_discount() == None
