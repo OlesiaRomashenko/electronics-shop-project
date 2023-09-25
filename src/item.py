f169220871d12debea40ca54890ccdaeaedbc63d
Olesia_Rom <olesya.einstein@gmail.com>
2023-09-25 19:48:21 +0200

    homework-2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +16,37 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, prod_name):
+        if len(prod_name) > 10:
+            self.__name = prod_name[:10]
+        else:
+            self.__name = prod_name
+
+    @classmethod
+    def instantiate_from_csv(cls, file_name):
+        with open(file_name) as f:
+            reader = csv.DictReader(f)
+            Item.all = []
+            for row in reader:
+                __name = row['name']
+                price = row['price']
+                quantity = row['quantity']
+                cls(__name, price, quantity)
+
+    @staticmethod
+    def string_to_number(num_str):
+        return int(float(num_str))
 
     def calculate_total_price(self) -> float:
         """
@@ -32,4 +61,4 @@ class Item:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price = self.price* self.pay_rate
+        self.price = self.price * self.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -10,10 +10,30 @@ def class_instance():
 
 
 def test_init(class_instance):
-    assert class_instance.name == "Смартфон"
+    assert class_instance._Item__name == "Смартфон"
     assert class_instance.price == 10000
     assert class_instance.quantity == 20
     assert class_instance is class_instance.__class__.all[0]
+
+
+def test_get_name(class_instance):
+    assert class_instance.name == "Смартфон"
+
+
+def test_set_name(class_instance):
+    class_instance.name = 'СуперСмартфон'
+    assert class_instance.name == "СуперСмарт"
+
+
+def test_instantiate_from_csv(class_instance):
+    class_instance.instantiate_from_csv('../src/items.csv')
+    assert len(class_instance.all) == 5
+
+
+def test_string_to_number(class_instance):
+    assert class_instance.string_to_number('5') == 5
+    assert class_instance.string_to_number('5.0') == 5
+    assert class_instance.string_to_number('5.5') == 5
 
 
 def test_calculate_total_price(class_instance):
